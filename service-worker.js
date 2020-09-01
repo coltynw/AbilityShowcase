3ef0e34ad0151c4198fc9154ce1341655ea3e97e
Coltyn2 <coltynwiewel@gmail.com>
2020-09-01 17:47:03 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/coltynw/abilityshowcase/precache-manifest.c4d81598559d8f61a71f23277f49204c.js"
+  "/abilityshowcase/precache-manifest.9f60e7f06b197da79b4aef1b57804506.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/coltynw/abilityshowcase/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/abilityshowcase/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
